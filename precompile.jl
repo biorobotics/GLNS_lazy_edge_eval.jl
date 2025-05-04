29fefa29370a63bfa4c9f3e4b56c8e25b13adccb
Anoop Bhat <noopy.g.bhat@gmail.com>
2025-05-04 16:49:16 -0400

    Updated precompilation files

--- precompile.jl
+++ precompile.jl
@@ -1,6 +1,6 @@
 using PackageCompiler
 using Pkg
-Pkg.activate(expanduser("~/GLNS_lazy_edge_eval.jl"))
+Pkg.activate(expanduser("~/GLNS_astar_repair.jl"))
 using GLNS
 
 # create_sysimage(["GLNS"]; sysimage_path="GLNS.so",precompile_execution_file="precompilation_script.jl")
