e597b24f2443f0d5a56b83a33bb18f9a9489a39b
Anoop Bhat <noopy.g.bhat@gmail.com>
2025-05-04 20:56:27 -0400

    Updated precompilation statements

--- precompilation_script.jl
+++ precompilation_script.jl
@@ -47,8 +47,9 @@ function main()
     @time GLNS.main(ARGS, 100., inf_val, given_initial_tours, false, "", dist, false)
     @time GLNS.main(PyList{Any}(ARGS), 100., inf_val, PyArray{Int64, 1, true, true, Int64}(given_initial_tours), false, "", PyArray{Int64, 2, true, true, Int64}(dist), false)
 
-    @time GLNS.main(ARGS, 100., inf_val, given_initial_tours, false, "", dist, true)
-    @time GLNS.main(PyList{Any}(ARGS), 100., inf_val, PyArray{Int64, 1, true, true, Int64}(given_initial_tours), false, "", PyArray{Int64, 2, true, true, Int64}(dist), true)
+    if i==1
+      @time GLNS.main(PyList{Any}(ARGS), 100., inf_val, PyArray{Int64, 1, true, true, Int64}(given_initial_tours), false, "", PyArray{Int64, 2, true, true, Int64}(dist), true)
+    end
   end
 end
 
