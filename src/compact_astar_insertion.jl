d3ce36f43f8e74345d59ed0f833ad1e60ac6454f
Anoop Bhat <noopy.g.bhat@gmail.com>
2025-05-05 11:05:06 -0400

    Added some commented code that computes h values

--- src/compact_astar_insertion.jl
+++ src/compact_astar_insertion.jl
@@ -58,13 +58,25 @@ function VDInfo(dist::AbstractArray{Int64, 2}, sets::Vector{Vector{Int64}}, memb
   return vd_info
 end
 
+# Note: this whole function assumes open tsp, doesn't account for cost of returning to depot.
+# Not a fundamental limitation, I just didn't implement it to handle closed TSP
 function astar_insertion!(sets_to_insert::Vector{Int64}, dist::AbstractArray{Int64, 2}, sets::Vector{Vector{Int64}}, membership::Vector{Int64}, inf_val::Int64, stop_time::Float64, vd_info::VDInfo, partial_tour::Vector{Int64}, known_feas_tour::Vector{Int64})
+  #=
+  # When the next unvisted index in partial_tour is tour_idx, the h value is dist[node_idx, partial_tour[tour_idx]] + h_vals[tour_idx]
+  h_vals = Vector{Int64}(undef, length(partial_tour))
+  h_vals[length(partial_tour)] = 0
+  for tour_idx=length(partial_tour)-1:-1:1
+    h_vals[tour_idx] = dist[partial_tour[tour_idx], partial_tour[tour_idx + 1]] + h_vals[tour_idx + 1]
+  end
+  =#
+
   closed_list = Set{Tuple{Int64, Vector{Bool}, Int64}}()
 
   # open_list = PriorityQueue{VDNode, Int64}()
   open_list = HeapPriorityQueue{VDNode, Int64}()
   # open_list = SortedVectorPriorityQueue{VDNode, Int64}()
   # open_list = VectorPriorityQueue{VDNode, Int64}()
+  # root_node = VDNode(1, Vector{VDNode}(), zeros(Bool, length(sets_to_insert)), 1, 0, dist[1, partial_tour[2]] + h_vals[2])
   root_node = VDNode(1, Vector{VDNode}(), zeros(Bool, length(sets_to_insert)), 1, 0, 0)
   enqueue!(open_list, root_node, 0)
 
@@ -225,7 +237,9 @@ function astar_insertion!(sets_to_insert::Vector{Int64}, dist::AbstractArray{Int
 
     for (removed_set_idx, node_idx) in zip(neighbor_removed_sets, neighbors)
       # bt = time_ns()
-      neighbor_node = VDNode(pop.tour_idx + 1, [pop], copy(pop.visited_removed_sets), node_idx, pop.g_val + dist[pop.final_node_idx, node_idx], 0)
+      # h_val = next_nonremoved_idx > length(partial_tour) ? 0 : dist[node_idx, partial_tour[next_nonremoved_idx]] + h_vals[next_nonremoved_idx]
+      h_val = 0
+      neighbor_node = VDNode(pop.tour_idx + 1, [pop], copy(pop.visited_removed_sets), node_idx, pop.g_val + dist[pop.final_node_idx, node_idx], h_val)
       if removed_set_idx != -1
         neighbor_node.visited_removed_sets[removed_set_idx] = true
         neighbor_node.key[2][removed_set_idx] = true
