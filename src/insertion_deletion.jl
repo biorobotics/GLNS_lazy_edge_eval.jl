7022f052556176f425cd769de46b187a71d442b5
Anoop Bhat <noopy.g.bhat@gmail.com>
2025-05-06 11:44:43 -0400

    If we get a tour cost larger than inf_val, check if any individual edge has inf cost. If so, throw error. Otherwise, terminate GLNS. Triangle inequality violation may cause us to rule out the current best tour

--- src/insertion_deletion.jl
+++ src/insertion_deletion.jl
@@ -111,7 +111,24 @@ function remove_insert_dp(current::Tour, best::Tour, dist::AbstractArray{Int64,2
   else
     trial.cost = tour_cost(trial.tour, dist)
     if trial.cost >= inf_val
-      throw("A* insertion gave infinite cost tour")
+      throw("DP insertion gave infinite cost tour")
+      # Check if we took an infinite cost edge. Since inf_val equals the cost of the incumbent + 1,
+      # it's possible that the triangle inequality violation makes DP give a higher-cost tour than the
+      # incumbent, and thus the total cost might be >= inf_val
+      inf_edge = false
+      for (node_idx1, node_idx2) in zip(trial.tour[1:end-1], trial.tour[2:end])
+        if dist[node_idx1, node_idx2] == inf_val
+          inf_edge = true
+          break
+        end
+      end
+      inf_edge |= (dist[trial.tour[end], trial.tour[1]] == inf_val)
+      if inf_edge
+        throw("DP insertion gave infinite cost tour")
+      else
+        trial = current
+        return trial, true
+      end
     end
   end
 
